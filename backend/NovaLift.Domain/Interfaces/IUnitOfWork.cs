0e9fec742b669adeab1c2321a3bc0c2aa6eebc3b
MNS <91156775+minyarzar@users.noreply.github.com>
2026-08-06 04:00:46 +0700

    Update IUnitOfWork.cs

--- backend/NovaLift.Domain/Interfaces/IUnitOfWork.cs
+++ backend/NovaLift.Domain/Interfaces/IUnitOfWork.cs
@@ -1,25 +1,44 @@
 using NovaLift.Domain.Entities;
-using NovaLift.Domain.Interfaces;
 
 namespace NovaLift.Domain.Interfaces;
 
 public interface IUnitOfWork : IDisposable
 {
     IRepository<User> Users { get; }
+
     IRepository<Wallet> Wallets { get; }
+
     IRepository<Transaction> Transactions { get; }
+
     IRepository<Product> Products { get; }
+
     IRepository<TaskItem> Tasks { get; }
+
+    // Add this
+    IRepository<TaskTemplate> TaskTemplates { get; }
+
     IRepository<Order> Orders { get; }
+
     IRepository<Country> Countries { get; }
+
     IRepository<Language> Languages { get; }
+
     IRepository<VipConfig> VipConfigs { get; }
+
     IRepository<Referral> Referrals { get; }
+
     IRepository<Commission> Commissions { get; }
+
     IRepository<Banner> Banners { get; }
+
     IRepository<Announcement> Announcements { get; }
+
     IRepository<PaymentMethodConfig> PaymentMethodConfigs { get; }
+
     IRepository<Setting> Settings { get; }
+
     IRepository<AuditLog> AuditLogs { get; }
+
+
     Task<int> SaveChangesAsync();
 }
