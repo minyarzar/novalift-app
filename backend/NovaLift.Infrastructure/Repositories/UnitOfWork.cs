82c9afab95c7b15512943e5dffe63ae67c60c5e4
MNS <91156775+minyarzar@users.noreply.github.com>
2026-08-06 04:05:38 +0700

    Update UnitOfWork.cs

--- backend/NovaLift.Infrastructure/Repositories/UnitOfWork.cs
+++ backend/NovaLift.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,41 +1,62 @@
-public interface IUnitOfWork : IDisposable
+public class UnitOfWork : IUnitOfWork
 {
-
-    IRepository<User> Users { get; }
-
-    IRepository<Wallet> Wallets { get; }
-
-    IRepository<Transaction> Transactions { get; }
-
-    IRepository<Product> Products { get; }
-
-    IRepository<TaskItem> Tasks { get; }
-
-    IRepository<TaskTemplate> TaskTemplates { get; }
+    private readonly AppDbContext _context;
+    private bool _disposed;
 
 
-    IRepository<Order> Orders { get; }
+    public IRepository<User> Users { get; }
+    public IRepository<Wallet> Wallets { get; }
+    public IRepository<Transaction> Transactions { get; }
+    public IRepository<Product> Products { get; }
+    public IRepository<TaskItem> Tasks { get; }
+    public IRepository<TaskTemplate> TaskTemplates { get; }
 
-    IRepository<Country> Countries { get; }
-
-    IRepository<Language> Languages { get; }
-
-    IRepository<VipConfig> VipConfigs { get; }
-
-    IRepository<Referral> Referrals { get; }
-
-    IRepository<Commission> Commissions { get; }
-
-    IRepository<Banner> Banners { get; }
-
-    IRepository<Announcement> Announcements { get; }
-
-    IRepository<PaymentMethodConfig> PaymentMethodConfigs { get; }
-
-    IRepository<Setting> Settings { get; }
-
-    IRepository<AuditLog> AuditLogs { get; }
+    public IRepository<Order> Orders { get; }
+    public IRepository<Country> Countries { get; }
+    public IRepository<Language> Languages { get; }
+    public IRepository<VipConfig> VipConfigs { get; }
+    public IRepository<Referral> Referrals { get; }
+    public IRepository<Commission> Commissions { get; }
+    public IRepository<Banner> Banners { get; }
+    public IRepository<Announcement> Announcements { get; }
+    public IRepository<PaymentMethodConfig> PaymentMethodConfigs { get; }
+    public IRepository<Setting> Settings { get; }
+    public IRepository<AuditLog> AuditLogs { get; }
 
 
-    Task<int> SaveChangesAsync();
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+
+
+        Users = new Repository<User>(_context);
+        Wallets = new Repository<Wallet>(_context);
+        Transactions = new Repository<Transaction>(_context);
+
+        Products = new Repository<Product>(_context);
+
+        Tasks = new Repository<TaskItem>(_context);
+
+        // ADD THIS
+        TaskTemplates = new Repository<TaskTemplate>(_context);
+
+
+        Orders = new Repository<Order>(_context);
+        Countries = new Repository<Country>(_context);
+        Languages = new Repository<Language>(_context);
+        VipConfigs = new Repository<VipConfig>(_context);
+        Referrals = new Repository<Referral>(_context);
+        Commissions = new Repository<Commission>(_context);
+        Banners = new Repository<Banner>(_context);
+        Announcements = new Repository<Announcement>(_context);
+        PaymentMethodConfigs = new Repository<PaymentMethodConfig>(_context);
+        Settings = new Repository<Setting>(_context);
+        AuditLogs = new Repository<AuditLog>(_context);
+    }
+
+
+    public Task<int> SaveChangesAsync()
+    {
+        return _context.SaveChangesAsync();
+    }
 }
